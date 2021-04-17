4ab33be330a18ef214dd03af8b3788c2367fc1bb
Whire-coder <romeo.pst44@gmail.com>
2021-04-18 01:54:06 +0200

    🖼️ Add avatars save

--- events/userUpdate.js
+++ events/userUpdate.js
@@ -6,7 +6,7 @@ module.exports = class {
 
 	async run (oldUser, newUser) {
 
-        // if(oldUser.avatar !== newUser.avatar) this.client.channels.cache.get('795147142011420722').send(newUser.displayAvatarURL({ dynamic:true, size: 1024}))
+        if(oldUser.avatar !== newUser.avatar) this.client.channels.cache.get('833126258757992459').send(newUser.displayAvatarURL({ dynamic: true }))
 		
 		if(oldUser.username !== newUser.username) {
 
