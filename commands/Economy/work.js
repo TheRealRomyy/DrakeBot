498d298e8e4484a8fa89555c73bc736ebe989817
Whire-coder <romeo.pst44@gmail.com>
2021-05-26 15:22:26 +0200

    🧰 Update work

--- commands/Economy/work.js
+++ commands/Economy/work.js
@@ -31,61 +31,71 @@ class Work extends Command {
                 name: "Developper",
                 pay: "05|10",
                 chance: "0|19",
-                number: 1
+                number: 1,
+                chanceStr: "19",
             }, 
             {
                 name: "Cooker",
                 pay: "04|09",
                 chance: "19|39",
-                number: 2
+                number: 2,
+                chanceStr: "20",
             },
             {
                 name: "Policeman",
                 pay: "06|11",
                 chance: "39|53",
-                number: 3
+                number: 3,
+                chanceStr: "14"
             }, 
             {
                 name: "Coach",
                 pay: "20|40",
                 chance: "53|63",
-                number: 4
+                number: 4,
+                chanceStr: "10"
             },
             {
                 name: "Astronaut",
                 pay: "100|1000",
                 chance: "63|64",
-                number: 5
+                number: 5,
+                chanceStr: "1"
             }, 
             {
                 name: "Chef",
                 pay: "25|35",
                 chance: "64|71",
-                number: 6
+                number: 6,
+                chanceStr: "7"
             },
             {
                 name: "Doctor",
                 pay: "40|75",
                 chance: "71|74",
-                number: 7
+                number: 7,
+                chanceStr: "3"
             },
             {
                 name: "Journalist",
                 pay: "30|40",
                 chance: "74|77",
-                number: 8
+                number: 8,
+                chanceStr: "3"
             },
             {
                 name: "Firefighter",
                 pay: "06|11",
                 chance: "77|94",
-                number: 9
+                number: 9,
+                chanceStr: "17"
             }, 
             {
                 name: "Dentist",
                 pay: "30|50",
                 chance: "94|101",
-                number: 10
+                number: 10,
+                chanceStr: "6"
             }
         ];
 
@@ -120,14 +130,20 @@ class Work extends Command {
         const win = client.functions.getRandomInt(parseInt(minToWin), parseInt(maxToWin));
         jobName = message.drakeWS("economy/work:" + jobName.toUpperCase());
 
+        let color = "BLUE";
+        if(win > 20) color = "ORANGE";
+        if(win > 40) color = "YELLOW";
+        if(win > 100) color = "BLACK";
+
         const embed = new MessageEmbed()
         .setAuthor(message.author.username, message.author.displayAvatarURL({ dynamic:true }))
-        .setColor(client.cfg.color.purple)
+        .setColor(color)
         .setFooter(client.cfg.footer)
         .setDescription(message.drakeWS("economy/work:WORK", {
             pay: win,
             job: jobName,
-            symbol: data.guild.symbol
+            symbol: data.guild.symbol,
+            chance: job.chanceStr
         }));
 
         message.channel.send(embed);
