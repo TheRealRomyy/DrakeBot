823e23af9898798e6f281f6583f2645f5ac42c4f
Whire-coder <romeo.pst44@gmail.com>
2021-07-17 14:03:17 +0200

    🥅 Fix error

--- events/messageReactionRemove.js
+++ events/messageReactionRemove.js
@@ -11,32 +11,35 @@ class MessageReactionRemove {
         if(!reaction || !user) return;
         
         if(user.bot) return;
-        let message = reaction.message
+        let message = reaction.message;
 
-        let emoji = reaction._emoji
+        let emoji = reaction._emoji;
         if(!emoji) return;
         let emojiName = emoji.name;
 
-        if(user.partial) await user.fetch()
-        if(message.partial) await message.fetch()
+        if(user.partial) await user.fetch();
+        if(message.partial) await message.fetch();
         if(!message) return;
 
-        let channel = message.channel
+        let channel = message.channel;
         if(!channel) return;
 
-        let member  = message.guild.members.cache.get(user.id)
-        if(member.partial) await member.fetch()
+        let member  = message.guild.members.cache.get(user.id);
         if(!member) return;
+        if(member.partial) await member.fetch();
 
         let guildData = await client.db.findOrCreateGuild(message.guild);
 
         if(guildData.reactionroles !== null) {
+
             let reactionRoleData = guildData.reactionroles.find(r => r.message === message.id && r.reaction === emoji.id || r.message === message.id && r.reaction === emoji.name);
 
             if(reactionRoleData) {
+
                 if(emojiName !== reactionRoleData.reaction && emoji.id !== reactionRoleData.reaction) return;
 
                 if(reactionRoleData.message === message.id && reactionRoleData.reaction === emoji.id || reactionRoleData.message === message.id && reactionRoleData.reaction === emoji.name) {
+
                     let role = message.guild.roles.cache.get(reactionRoleData.role);
                     if(role) member.roles.remove(role);
                 };
