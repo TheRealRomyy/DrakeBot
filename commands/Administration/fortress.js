a3d9ae221ed046c559ca96e90025d3a78442c0ba
Whire-coder <romeo.pst44@gmail.com>
2021-08-07 00:21:36 +0200

    🏰 Update fortress command

--- commands/Administration/fortress.js
+++ commands/Administration/fortress.js
@@ -1,4 +1,5 @@
 const Command = require("../../structure/Commands.js");
+const { MessageButton, MessageActionRow } = require("discord.js");
 
 class Fortress extends Command {
 
@@ -6,34 +7,36 @@ class Fortress extends Command {
         super(client, {
             name: "fortress",
             aliases: ["forteresse"],
-            enabled: false,
+            enabled: true,
             dirname: __dirname,
             botPerms: [ "ADMINISTRATOR" ],
             userPerms: [ "ADMINISTRATOR" ],
-            restriction: []
+            restriction: [],
+
+            slashCommandOptions: {
+                description: "🏰 Toogle fortress mode (user can't join)"
+            }
         });
     };
 
     async run(message, args, data) {
 
-        let filter = (reaction, user) => {
-            return ['👍', '👎'].includes(reaction.emoji.name) && user.id === message.author.id;
-        };
+        let filter = null;
         let confirmationMsg = null;
 
-        async function WaitForReaction(msg) {
+        async function WaitForButtons(msg) {
 
-            let reaction = null;
+            let button = null;
     
-            await msg.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] }).then(collected => {
-                reaction = collected.first();
-                reaction.users.remove(message.author.id).catch(() => {});
+            await msg.awaitMessageComponent({ filter, max: 1, time: 60000, errors: ['time'] }).then(async collected => {
+                button = collected;
+                await button.deferUpdate();
             }).catch(collected => {
                 return cancel();
             });
     
-            if(reaction == null) return;
-            return reaction.emoji.name;
+            if(button == null) return;
+            return button.customId;
         };
 
         async function cancel() {
@@ -51,16 +54,35 @@ class Fortress extends Command {
             });
 
         } else {
-            confirmationMsg = await message.channel.send(message.drakeWS("administration/fortress:CONFIRMATION", {
-                emoji: "question"
-            }));
+            let yesButton = new MessageButton()
+                .setStyle("SUCCESS")
+                .setLabel('Yes 👍')
+                .setDisabled(false)
+                .setCustomId(`${message.guild.id}${message.author.id}${Date.now()}YES`);
 
-            await confirmationMsg.react('👍');
-            await confirmationMsg.react('👎');
+            let noButton = new MessageButton()
+                .setStyle("DANGER")
+                .setLabel('No 👎')
+                .setDisabled(false)
+                .setCustomId(`${message.guild.id}${message.author.id}${Date.now()}NO`);
 
-            let reaction = await WaitForReaction(confirmationMsg);
+            let group1 = new MessageActionRow().addComponents([ yesButton, noButton ]);
 
-            if(reaction == '👍') {
+            filter = (button) => button.user.id === message.author.id && (
+                button.customId === yesButton.customId ||
+                button.customId === noButton.customId
+            );
+
+            confirmationMsg = await message.channel.send({
+                content: message.drakeWS("administration/fortress:CONFIRMATION", {
+                    emoji: "question"
+                }),
+                components: [group1]
+            });
+
+            let button = await WaitForButtons(confirmationMsg);
+
+            if(button === yesButton.customId) {
 
                 confirmationMsg.delete().catch(() => {});
 
@@ -71,11 +93,107 @@ class Fortress extends Command {
                     emoji: "succes"
                 });
 
-            } else if(reaction == '👎') {
+            } else if(button === noButton.customId) {
                 confirmationMsg.delete().catch(() => {});
 
                 return message.drake("common:CANCEL", {
                     emoji: "succes"
+                });
+            } else {
+                return cancel()
+            };
+        };
+    };
+
+    async runInteraction(interaction, data) {
+
+        await interaction.reply({
+            content: "** **"
+        });
+
+        let filter = null;
+        let confirmationMsg = null;
+
+        async function WaitForButtons(msg) {
+
+            let button = null;
+    
+            await msg.awaitMessageComponent({ filter, max: 1, time: 60000, errors: ['time'] }).then(async collected => {
+                button = collected;
+                await button.deferUpdate();
+            }).catch(collected => {
+                return cancel();
+            });
+    
+            if(button == null) return;
+            return button.customId;
+        };
+
+        async function cancel() {
+            confirmationMsg.delete().catch(() => {});
+        };
+
+        if(data.guild.fortress) {
+
+            data.guild.fortress = false;
+            await data.guild.save();
+
+            return interaction.editReply({
+                content: interaction.drakeWS("administration/fortress:SUCCES_DISABLED", {
+                    emoji: "succes"
+                })
+            });
+
+        } else {
+            let yesButton = new MessageButton()
+                .setStyle("SUCCESS")
+                .setLabel('Yes 👍')
+                .setDisabled(false)
+                .setCustomId(`${interaction.guild.id}${interaction.user.id}${Date.now()}YES`);
+
+            let noButton = new MessageButton()
+                .setStyle("DANGER")
+                .setLabel('No 👎')
+                .setDisabled(false)
+                .setCustomId(`${interaction.guild.id}${interaction.user.id}${Date.now()}NO`);
+
+            let group1 = new MessageActionRow().addComponents([ yesButton, noButton ]);
+
+            filter = (button) => button.user.id === interaction.user.id && (
+                button.customId === yesButton.customId ||
+                button.customId === noButton.customId
+            );
+
+            confirmationMsg = await interaction.channel.send({
+                content: interaction.drakeWS("administration/fortress:CONFIRMATION", {
+                    emoji: "question"
+                }),
+                components: [group1]
+            });
+
+            let button = await WaitForButtons(confirmationMsg);
+
+            if(button === yesButton.customId) {
+
+                confirmationMsg.delete().catch(() => {});
+
+                data.guild.fortress = true;
+                await data.guild.save()
+
+                return interaction.editReply({
+                    content: interaction.drakeWS("administration/fortress:SUCCES", {
+                        emoji: "succes"
+                    })
+                });
+
+            } else if(button === noButton.customId) {
+                confirmationMsg.delete().catch(() => {});
+
+                return interaction.editReply({
+                    content: interaction.drakeWS("common:CANCEL", {
+                        emoji: "succes"
+                    }),
+                    ephemeral: true
                 });
             } else {
                 return cancel()
