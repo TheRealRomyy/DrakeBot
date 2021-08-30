f498574d79f2cdfc71f370ccec9f37e515f9cb18
Whire-coder <romeo.pst44@gmail.com>
2021-08-30 22:04:54 +0200

    🐛 Fix small bugs

--- commands/Social/feed.js
+++ commands/Social/feed.js
@@ -23,7 +23,7 @@ class Feed extends Command {
 
         let user = this.client.user;
 
-        if(args[0]) user = message.mentions.users.first() || (args[0] ? await this.client.users.fetch(args[0]) : null);
+        if(args[0]) user = message.mentions.users.first() || (args[0] && args[0] !== "random" ? await this.client.users.fetch(args[0]) : null);
         if(!user) user = this.client.user;
 
         if(args[0] === "random") user = message.guild.members.cache.random(5).filter(member => member.user.id !== message.author.id)[0].user;
