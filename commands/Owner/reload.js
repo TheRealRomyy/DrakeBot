d48f5c44d35865b874d6c805aa99ffa8dd197a3a
Whire-coder <romeo.pst44@gmail.com>
2021-10-10 18:39:00 +0200

    ⚙️ Update reload command

--- commands/Owner/reload.js
+++ commands/Owner/reload.js
@@ -25,76 +25,87 @@ class Reload extends Command {
 
         if(args[0]) {
 
-            if(args[0] === "languages") {
-                const languages = require("../../helpers/lang");
-                client.translations = await languages();
-
-                client.logger.log("Languages: reloaded");
-                const languageMessage = await message.channel.send({
-                    content: `${client.emotes.succes} **Languages reloaded !**`
-                });
-
-                setTimeout(() => {
-                    message.delete().catch(() => {});
-                    languageMessage.delete().catch(() => {});
-                }, 3000);
-
-            } else if(args[0] === "commands") {
-                client.cmds.forEach(async (cmd) => {
-                    await client.unloadCommand(cmd.settings.location, cmd.help.name);
-                    await client.loadCommand(cmd.settings.location, cmd.help.name);
-                });
-
-                const commandsMessage = await message.channel.send({
-                    content: `${client.emotes.succes} **All commands reloaded !**`
-                });
-
-                setTimeout(() => {
-                    message.delete().catch(() => {});
-                    commandsMessage.delete().catch(() => {});
-                }, 3000);
-
-            } else if(args[0] === "events") {
-
-                for (const [eventName, eventFunction] of Object.entries(client._events)) {
-                    client.off(eventName, eventFunction);
-                };
-                  
-                const evtFiles = await readdir("./events/");
-                evtFiles.forEach((file) => {
-                    const eventName = file.split(".")[0];
-                    const event = new (require(`../../events/${file}`))(client);
-                    client.logger.log(`Event '${eventName}' successfully loaded`);
-                    client.on(eventName, (...args) => event.run(...args));
-                    delete require.cache[require.resolve(`../../events/${file}`)];
-                }); 
-
-                const eventMessages = await message.channel.send({
-                    content: `${client.emotes.succes} **All events reloaded !**`
-                });
-
-                setTimeout(() => {
-                    message.delete().catch(() => {});
-                    eventMessages.delete().catch(() => {});
-                }, 3000);
-            } else {
-                const cmd = client.cmds.get(args[0]) || client.cmds.get(client.aliases.get(args[0]));
-                if(!cmd) return message.channel.send({
-                    content: `${client.emotes.error} **La commande __${args[0]}__ n'existe pas !**`
-                });
-                
-                await client.unloadCommand(cmd.settings.location, cmd.help.name);
-                await client.loadCommand(cmd.settings.location, cmd.help.name);
-        
-                const commandMessage = await message.channel.send({
-                    content: `${client.emotes.succes} **Command __${cmd.help.name}__ reloaded !**`
-                });
+            switch(args[0]) {
+                case "languages":
+                    const languages = require("../../helpers/lang");
+                    client.translations = await languages();
     
-                setTimeout(() => {
-                    message.delete().catch(() => {});
-                    commandMessage.delete().catch(() => {});
-                }, 3000);
-            };
+                    client.logger.log("Languages: reloaded");
+                    const languageMessage = await message.channel.send({
+                        content: `${client.emotes.succes} **Languages reloaded !**`
+                    });
+    
+                    setTimeout(() => {
+                        message.delete().catch(() => {});
+                        languageMessage.delete().catch(() => {});
+                    }, 3000);
+                    break;
+                case "commands":
+                    client.cmds.forEach(async (cmd) => {
+                        await client.unloadCommand(cmd.settings.location, cmd.help.name);
+                        await client.loadCommand(cmd.settings.location, cmd.help.name);
+                    });
+    
+                    const commandsMessage = await message.channel.send({
+                        content: `${client.emotes.succes} **All commands reloaded !**`
+                    });
+    
+                    setTimeout(() => {
+                        message.delete().catch(() => {});
+                        commandsMessage.delete().catch(() => {});
+                    }, 3000);
+                    break;
+                case "events":
+                    for (const [eventName, eventFunction] of Object.entries(client._events)) {
+                        client.off(eventName, eventFunction);
+                    };
+                      
+                    const evtFiles = await readdir("./events/");
+                    evtFiles.forEach((file) => {
+                        const eventName = file.split(".")[0];
+                        const event = new (require(`../../events/${file}`))(client);
+                        client.logger.log(`Event '${eventName}' successfully loaded`);
+                        client.on(eventName, (...args) => event.run(...args));
+                        delete require.cache[require.resolve(`../../events/${file}`)];
+                    }); 
+    
+                    const eventMessages = await message.channel.send({
+                        content: `${client.emotes.succes} **All events reloaded !**`
+                    });
+    
+                    setTimeout(() => {
+                        message.delete().catch(() => {});
+                        eventMessages.delete().catch(() => {});
+                    }, 3000);
+                    break;
+                default:
+                    const cmd = client.cmds.get(args[0]) || client.cmds.get(client.aliases.get(args[0]));
+                    const event = Boolean(Object.keys(client._events).includes(args[0]));
+
+                    if(!cmd && !event) return message.channel.send({
+                        content: `${client.emotes.error} **La commande ou l'event __${args[0]}__ n'existe pas !**`
+                    });
+
+                    if(cmd) {
+                        await client.unloadCommand(cmd.settings.location, cmd.help.name);
+                        await client.loadCommand(cmd.settings.location, cmd.help.name);
+                    } else {
+                        const event = new (require(`../../events/${args[0]}.js`))(client);
+
+                        client.off(args[0], client._events[args[0]]);
+                        client.on(args[0], (...args) => event.run(...args));
+                    }
+                    
+                    const specificReloadMessage = await message.channel.send({
+                        content: `${client.emotes.succes} **${cmd ? "Command" : "Event"} __${cmd ? cmd.help.name : args[0]}__ successfuly reloaded !**`
+                    });
+        
+                    setTimeout(() => {
+                        message.delete().catch(() => {});
+                        specificReloadMessage.delete().catch(() => {});
+                    }, 3000);
+                    break;
+            }
 
         } else {
 
